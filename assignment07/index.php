a8266d6d818d594c9183a6ae5e8a8891bef88e88
Jen <jen.rodasky@gmail.com>
2020-02-01 07:57:01 -0600

    Create index.php
    Completed assignment

---
+++ assignment07/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Assignment 7</title>
+  <meta charset="utf-8">
+</head>
+<body>
+<!-- Jen Cink
+     index.php
+     INFO2340.WW
+     Theondels
+     1/29/2020
+-->
+<?php 
+  //define your variables here.
+  $dinner = 'Chicken Cordon Bleu';
+  $stars = '4';
+  $favPlace = 'the new house';
+  $favAnimal = 'zebra';
+?>
+
+
+<h1>Good morning, Nebraska!</h1>
+<div>Last night I ate <?php echo $dinner ?>.</div>
+<div>While it was good, it wasn't perfect. I'd give it <?php echo $stars ?> stars out of 4.</div>
+<div>Tomorrow I plan to go to <?php echo $favPlace ?>.</div>
+<div>My favorite animal is the <?php echo $favAnimal ?>.</div>
+
+</body>
+</html>
